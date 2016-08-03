fb38b7ee51aaa0413d0fc50e58db9a901b6317fc
Latasha <lellis9@cps.edu>
2016-08-03 18:02:48 +0000

    updates

---
+++ pow.js
@@ -0,0 +1,22 @@
+// JavaScript File
+
+<div id="wufoo-z1sfgyae060k6dq">
+Fill out my <a href="https://tmitchell007.wufoo.com/forms/z1sfgyae060k6dq">online form</a>.
+</div>
+<div id="wuf-adv" style="font-family:inherit;font-size: small;color:#a7a7a7;text-align:center;display:block;">The easy to use <a href="http://www.wufoo.com/form-builder/">Wufoo form builder</a> helps you make forms easy, fast, and fun.</div>
+<script type="text/javascript">var z1sfgyae060k6dq;(function(d, t) {
+var s = d.createElement(t), options = {
+'userName':'tmitchell007',
+'formHash':'z1sfgyae060k6dq',
+'autoResize':true,
+'height':'948',
+'async':true,
+'host':'wufoo.com',
+'header':'show',
+'ssl':true};
+s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+s.onload = s.onreadystatechange = function() {
+var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+try { z1sfgyae060k6dq = new WufooForm();z1sfgyae060k6dq.initialize(options);z1sfgyae060k6dq.display(); } catch (e) {}};
+var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})(document, 'script');</script>
